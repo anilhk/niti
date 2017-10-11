1b58f28c4332f9ef6402e63829b5e553548ed477
Anil Karamchandani <YC03AK1@federated.fds>
2017-10-11 14:54:46 -0700

    second commit

--- src/main/java/com/niti/NitiApplication.java
+++ src/main/java/com/niti/NitiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NitiApplication {
 
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(NitiApplication.class, args);
 	}
